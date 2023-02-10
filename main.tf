f204730ad081b001635b1a1f0bbd549a89f6fb8c
ccseyhan <cemalcan.seyhan@deop.ca>
2023-02-10 15:22:58 -0300

    server config

--- main.tf
+++ main.tf
@@ -42,8 +42,8 @@ resource "aws_iam_instance_profile" "test_profile" {
 resource "aws_instance" "jenkins_ec2" {
   ami           = var.ami
   instance_type = var.instance_type
-  key_name = ""
-  vpc_security_group_ids = []
+  key_name = var.key_name
+  vpc_security_group_ids = [aws_security_group.jenkins_security_group.id]
   iam_instance_profile = aws_iam_instance_profile.test_profile.name
   tags = {
     Name = var.tag
@@ -52,28 +52,38 @@ resource "aws_instance" "jenkins_ec2" {
 }
 
 resource "aws_security_group" "jenkins_security_group" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.main.id
+  name        = "jenkins-sg"
 
   ingress {
-    description      = "TLS from VPC"
-    from_port        = 443
-    to_port          = 443
+    description      = "http"
+    from_port        = 0
+    to_port          = 80
     protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.main.cidr_block]
-    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
+   ingress {
+    description      = "ssh"
+    from_port        = 0
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+   ingress {
+    description      = "8080forjenkins"
+    from_port        = 0
+    to_port          = 8080
+    protocol         = "tcp"
+  }
   egress {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
 
   tags = {
     Name = "allow_tls"
   }
+}
 }
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "key_name" {
+    default = "tes1key"
+}
+variable "ami" {
+    default = "ami-026b57f3c383c2eec"
+}
+
+variable "instance_type" {
+ defdefault = "t3a.medium" 
+}
+
+variable "tag" {
+    default = "jenkins_server"
+}
+
